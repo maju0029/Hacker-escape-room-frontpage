b8ed65073a563630285e5eed469e04f94f937434
maju0029 <maju0029@gmail.com>
2025-11-07 15:04:25 +0100

    Changes to the mobile menu, mostly css some js

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 const hamMenu = document.querySelector('.ham-menu');
-
 const offScreenMenu = document.querySelector('.off-screen-menu');
+const overlay = document.querySelector(".overlay");
 
 hamMenu.addEventListener('click', () =>{
     hamMenu.classList.toggle('active');
     offScreenMenu.classList.toggle('active');
+    overlay.classList.toggle('active');
+})
+
+  overlay.addEventListener("click", () => {
+  offScreenMenu.classList.remove("active");
+  overlay.classList.remove("active");
+  hamMenu.classList.remove('active');
 })
